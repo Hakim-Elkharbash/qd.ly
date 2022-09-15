4b7091c501f830ccff6173cf9a8405287e371859
Hakim Elkharbash <libyapages@gmail.com>
2022-09-15 13:28:16 -0400

    Update db_new.php

--- db_new.php
+++ db_new.php
@@ -1,8 +1,8 @@
 <?
 $libyapages_host="localhost";
-$libyapages_dbname="bigly_store"; 
-$libyapages_username="bigly_libyapages"; 
-$libyapages_password="?!^df09oDAaO"; 
+$libyapages_dbname=""; 
+$libyapages_username=""; 
+$libyapages_password=""; 
 mysql_connect("$libyapages_host", "$libyapages_username", "$libyapages_password")or die("Could not connect to the server!"); 
 mysql_select_db("$libyapages_dbname")or die("Could not select database!"); 
 mysql_query("SET NAMES 'utf8'"); 
